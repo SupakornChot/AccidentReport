06af535e3904fc6ae83bf4f0e007add008f96ba0
artcheeze <surasak.jaikum@mail.kmutt.ac.th>
2017-04-09 22:51:31 +0700

    Add files via upload

--- Accident Report/src/ReportInterface.java
+++ Accident Report/src/ReportInterface.java
@@ -1,7 +1,24 @@
 
+
+import static java.awt.Event.INSERT;
+import java.awt.EventQueue;
+import java.awt.HeadlessException;
+import static java.awt.SystemColor.window;
+import java.beans.Statement;
 import java.io.File;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static javax.accessibility.AccessibleRole.TABLE;
+import javax.swing.ButtonGroup;
+import static javax.swing.DropMode.INSERT;
+import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import static jdk.nashorn.internal.runtime.ECMAException.CREATE;
+import sun.security.util.SecurityConstants.AWT;
+
+
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,12 +35,14 @@ public class ReportInterface extends javax.swing.JFrame {
     /**
      * Creates new form KmuttAcc
      */
+   
+
     public ReportInterface() {
         initComponents();
     }
     File selectedFile;
-    public static String Type,Loca,Name,Tel;
-    public static int id=0;
+    protected static String Type,Loca,Name,Tel;
+    protected static int id=0,Level=0;
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -48,6 +67,10 @@ public class ReportInterface extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jToggleButton1 = new javax.swing.JToggleButton();
         namepic = new javax.swing.JLabel();
+        cb1 = new javax.swing.JCheckBox();
+        cb2 = new javax.swing.JCheckBox();
+        cb3 = new javax.swing.JCheckBox();
+        jLabel7 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
@@ -90,51 +113,106 @@ public class ReportInterface extends javax.swing.JFrame {
         namepic.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         namepic.setText(". . . ");
 
+        cb1.setBackground(new java.awt.Color(255, 255, 216));
+        cb1.setText("ช่วยเหลือตัวเองได้");
+        cb1.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                cb1StateChanged(evt);
+            }
+        });
+        cb1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                cb1MouseClicked(evt);
+            }
+        });
+        cb1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cb1ActionPerformed(evt);
+            }
+        });
+        cb1.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                cb1PropertyChange(evt);
+            }
+        });
+
+        cb2.setBackground(new java.awt.Color(255, 255, 216));
+        cb2.setText("มีสติ แต่ขยับไม่ได้");
+        cb2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cb2ActionPerformed(evt);
+            }
+        });
+
+        cb3.setBackground(new java.awt.Color(255, 255, 216));
+        cb3.setText("หมดสติ เสียเลือดมาก");
+        cb3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cb3ActionPerformed(evt);
+            }
+        });
+
+        jLabel7.setText("ระดับความรุนแรง");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(25, 25, 25)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(namepic, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addGap(169, 169, 169)
+                                .addGap(194, 194, 194)
                                 .addComponent(jLabel4))
                             .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addGap(161, 161, 161)
+                                .addGap(186, 186, 186)
                                 .addComponent(jLabel5))
                             .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addGap(149, 149, 149)
-                                .addComponent(jLabel2))
-                            .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addGap(135, 135, 135)
+                                .addGap(160, 160, 160)
                                 .addComponent(jToggleButton1))
                             .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addGap(90, 90, 90)
+                                .addGap(115, 115, 115)
                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel6)
                                     .addComponent(jLabel3)))
                             .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addGap(122, 122, 122)
+                                .addGap(147, 147, 147)
                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(TypeT)
                                     .addComponent(LocaT)
                                     .addComponent(NameT)
                                     .addComponent(TelT, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(namepic, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(174, 174, 174)
+                        .addComponent(jLabel2))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addComponent(cb1)
+                        .addGap(151, 151, 151)
+                        .addComponent(cb3))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(155, 155, 155)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(cb2)
+                            .addComponent(jLabel7))))
+                .addContainerGap(22, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(23, 23, 23)
                 .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(TypeT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29)
+                .addGap(13, 13, 13)
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(LocaT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -148,11 +226,18 @@ public class ReportInterface extends javax.swing.JFrame {
                 .addComponent(TelT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(namepic)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addComponent(jToggleButton1)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel6)
-                .addGap(19, 19, 19))
+                .addGap(27, 27, 27)
+                .addComponent(jLabel7)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cb1)
+                    .addComponent(cb2)
+                    .addComponent(cb3))
+                .addContainerGap(35, Short.MAX_VALUE))
         );
 
         jLabel1.setFont(new java.awt.Font("Century Gothic", 0, 24)); // NOI18N
@@ -185,19 +270,19 @@ public class ReportInterface extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(32, 32, 32)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(34, 34, 34)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(46, 46, 46)
                 .addComponent(jToggleButton2)
                 .addGap(28, 28, 28))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(68, 68, 68))
+            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -206,9 +291,9 @@ public class ReportInterface extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(14, 14, 14)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 34, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
                     .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jToggleButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
@@ -222,42 +307,66 @@ public class ReportInterface extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, 0)
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
-        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void NameTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NameTActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_NameTActionPerformed
-
+   
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        if(cb1.isSelected()){
+            Level = 1;
+        }
+        else if(cb2.isSelected()){
+            Level = 2;
+        }
+        else if(cb3.isSelected()){
+            Level = 3;
+        }
         
+        
+        
+
         id++;
         Type = TypeT.getText();
         Loca = LocaT.getText();
         Name = NameT.getText();
         Tel = TelT.getText();
+        
         //ไฟล์รูปเอาจากตัวแปล selectedFile 
         
-        JOptionPane.showMessageDialog(TypeT, "เสร็จสิ้นการแจ้ง");
         
+      
+        try{
         System.out.println("Accident ID = "+id);
         System.out.println("Type = "+Type);
         System.out.println("Location = "+Loca);
         System.out.println("Name = "+Name);
         System.out.println("Tel number = "+Tel);
+        System.out.println("Level =  "+Level);
         System.out.println("File pic = "+selectedFile.getName());
-        
+        }catch(NullPointerException e){
+         
+        }finally{
+        JOptionPane.showMessageDialog(TypeT, "เสร็จสิ้นการแจ้ง");
         TypeT.setText("");
         LocaT.setText("");
         NameT.setText("");
         TelT.setText("");
         namepic.setText("");
+        cb1.setSelected(false);
+        cb2.setSelected(false);
+        cb3.setSelected(false);
+    }
+        
+        
+        
+        
+        
          
                 
     }//GEN-LAST:event_jButton2ActionPerformed
@@ -275,15 +384,51 @@ public class ReportInterface extends javax.swing.JFrame {
     }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     private void jToggleButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton2ActionPerformed
-        //StatusJFrame frame2 = new StatusJFrame(); //สำหรับลิ้งหน้าต่อไป(Error เพราะต้องรอ New หน้าใหม่)
-       // ReportInterface.this.setVisible(false); // ทำให้หน้าปัจจุบันหายไป เมื่อขึ้นหน้าใหม่
-        //frame2.setVisible(true); // ทำให้หน้าใหม่ มองเห็น
+      // NewJFrame frame2 = new NewJFrame(); //สำหรับลิ้งหน้าต่อไป(Error เพราะต้องรอ New หน้าใหม่)
+      // ReportInterface.this.setVisible(true); // ทำให้หน้าปัจจุบันหายไป เมื่อขึ้นหน้าใหม่
+       //frame2.setVisible(true); // ทำให้หน้าใหม่ มองเห็น
     }//GEN-LAST:event_jToggleButton2ActionPerformed
+
+    private void cb1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cb1ActionPerformed
+            ButtonGroup group = new ButtonGroup();
+             group.add(cb1);
+             group.add(cb2);
+             group.add(cb3);
+            
+ 
+
+    }//GEN-LAST:event_cb1ActionPerformed
+
+    private void cb1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cb1MouseClicked
+       
+    }//GEN-LAST:event_cb1MouseClicked
+
+    private void cb1PropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_cb1PropertyChange
+            
+    }//GEN-LAST:event_cb1PropertyChange
+
+    private void cb1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_cb1StateChanged
+       
+    }//GEN-LAST:event_cb1StateChanged
+
+    private void cb2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cb2ActionPerformed
+         ButtonGroup group = new ButtonGroup();
+             group.add(cb1);
+             group.add(cb2);
+             group.add(cb3);
+    }//GEN-LAST:event_cb2ActionPerformed
+
+    private void cb3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cb3ActionPerformed
+         ButtonGroup group = new ButtonGroup();
+             group.add(cb1);
+             group.add(cb2);
+             group.add(cb3);
+    }//GEN-LAST:event_cb3ActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[]){
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -307,20 +452,27 @@ public class ReportInterface extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
-
+    
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new ReportInterface().setVisible(true);
+
             }
+            
+
         });
     }
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField LocaT;
     private javax.swing.JTextField NameT;
     private javax.swing.JTextField TelT;
     private javax.swing.JTextField TypeT;
+    private javax.swing.JCheckBox cb1;
+    private javax.swing.JCheckBox cb2;
+    private javax.swing.JCheckBox cb3;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -330,6 +482,7 @@ public class ReportInterface extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JToggleButton jToggleButton1;
